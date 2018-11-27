8dafbb468cebe332ef9af9583b02df11c9485fbf
liangxuecong <497513735@qq.com>
2018-11-27 18:53:10 +0800

    11-27公司开发 1.增加 common.scss 2.开发首页

---
+++ src/components/indexModule.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="index-module" :class="[customClass]">
+    <div class="index-module-hd">
+      <span class="hd-txt">{{ title }}</span>
+      <span class="btn-more">更多></span>
+    </div>
+    <div class="index-module-bd">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexModule',
+  props: {
+    title: String,
+    customClass: String,
+    moreCb: Function,
+    data: Array|Object 
+  },
+  data() {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.index-module {
+  width: 100%;
+  .index-module-hd {
+    height: 120rpx;
+    line-height: 120rpx;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .hd-txt {
+      font-size: 36rpx;
+      color: $--color-font-black;
+    }
+
+    .btn-more {
+      padding: 0 30rpx;
+      height: 50rpx;
+      line-height: 50rpx;
+      background-color: $--color-theme;
+      font-size: 24rpx;
+      color: #fff;
+      border-radius: 25rpx;
+    }
+  }
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,46 +1,93 @@
 <template>
-  <div class="container" @click="clickHandle('test click', $event)">
-
-    <div class="userinfo" @click="bindViewTap">
-      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
-      <div class="userinfo-nickname">
-        <card :text="userInfo.nickName"></card>
+  <div class="p-wrap p-index">
+    <!-- 顶部容器 -->
+    <div class="index-top-wrap">
+      <div class="l-wrap" style="width:130rpx;">
+        <span class="cald-block common-radius common-shadow"></span>
+        <span class="search-block common-radius pr common-shadow">
+          <i class="i-search2 common-icon ver-center"></i>
+        </span>
+      </div>
+      <div class="banner-wrap common-radius common-shadow">
+        <image 
+          src="https://7863-xctest1-62e0ce-1257796632.tcb.qcloud.la/banner/banner.png?sign=1d90af401350ed567137ac3332d50eb0&t=1543308940"
+          mode="scaleToFill"/>
       </div>
     </div>
 
-    <div class="usermotto">
-      <div class="user-motto">
-        <card :text="motto"></card>
-      </div>
+    <!-- 快速通道容器 -->
+    <div class="hyperchannel-wrap">
+      <span class="hyperchannel-item">{{motto}}</span>
+      <span class="hyperchannel-item"></span>
+      <span class="hyperchannel-item"></span>
+      <span class="hyperchannel-item"></span>
     </div>
 
-    <form class="form-container">
-      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
-    </form>
+    <!--各大模块容器-->
+    <div class="modules-wrap">
+      <!-- m-精彩阅读 -->
+      <indexModule
+        custom-class="im-reading"
+        title="精彩阅读"
+        :data="imReadingData">
+        <div style="height:360rpx;" slot>
+          <swiper
+            style="height: 100%;"
+            :interval="1000"
+            next-margin="100rpx">
+            <block v-for="(item, index) in data" :key="index">
+              <swiper-item>
+                <image 
+                  class="swiper-img common-radius"
+                  :src="item.img"
+                  width="600"
+                  mode="scaleToFill" 
+                  height="360"/>
+              </swiper-item>
+            </block>
+          </swiper>
+        </div>  
+      </indexModule>
+      <!-- m-明星宠物 -->
+    </div>
   </div>
 </template>
 
 <script>
-import card from '@/components/card'
+import card from '@/components/card';
+import indexModule from '@/components/indexModule';
+
+
+//数据库
+const db = wx.cloud.database();
 
 export default {
   data () {
     return {
       motto: 'Hello World',
-      userInfo: {}
+      userInfo: {},
+      imReadingData: [{
+        type: '热门推荐',
+        title: '宠物天下',
+        img: 'https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=2645270468,2305942404&fm=173&app=49&f=JPEG?w=218&h=146&s=A2A1DB0596EB73110F35ADB703001012',
+        personNum: 3286
+      }, {
+        type: '热门推荐11',
+        title: '宠物天下22',
+        img: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=1526156770,4220481624&fm=173&app=49&f=JPEG?w=218&h=146&s=304348B4EE2E029EA84B71830300A088'
+      }]
     }
   },
 
   components: {
-    card
+    card, 
+    indexModule
   },
 
   methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      wx.navigateTo({ url })
-    },
+    /**
+     * @desc 获取用户信息
+     */
     getUserInfo () {
       // 调用登录接口
       wx.login({
@@ -53,8 +100,29 @@ export default {
         }
       })
     },
-    clickHandle (msg, ev) {
-      console.log('clickHandle:', msg, ev)
+
+    /**
+     * @desc 测试云函数
+     */
+    testCloud() {
+      //查询数据
+      db.collection('dog').get({
+        success: function (res) {
+          // res.data 包含该记录的数据
+          console.log(res)
+        }
+      });
+
+      //测试云函数
+      wx.cloud.callFunction({
+        name: 'srcCloud',
+        data: {
+          a: 123
+        }
+      })
+      .then(resp => {
+        console.log(resp);
+      });
     }
   },
 
@@ -62,56 +130,95 @@ export default {
     // 调用应用实例的方法获取全局数据
     this.getUserInfo();
 
-    const db = wx.cloud.database();
-
-    //查询数据
-    db.collection('dog').get({
-      success: function (res) {
-        // res.data 包含该记录的数据
-        console.log(res)
-      }
-    });
-
-    wx.cloud.callFunction({
-      name: 'srcCloud',
-      data: {
-        a: 123
-      }
-    })
-    .then(resp => {
-      console.log(resp);
-    });
+    this.testCloud();
+    
   }
 }
 </script>
 
-<style scoped>
-.userinfo {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+<style lang="scss">
+.p-wrap.p-index {
+  > div {
+    width: 100%;
 
-.userinfo-avatar {
-  width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
-  border-radius: 50%;
-}
+  }
 
-.userinfo-nickname {
-  color: #aaa;
-}
+  .index-top-wrap {
+    height: 200rpx;
+    display: flex;
+    justify-content:space-between;
+    margin-bottom: 14rpx;
 
-.usermotto {
-  margin-top: 150px;
-}
+    .l-wrap {
+      display:flex;
+      flex-direction:column;
+      justify-content:space-between;
+    }
 
-.form-control {
-  display: block;
-  padding: 0 12px;
-  margin-bottom: 5px;
-  border: 1px solid #ccc;
+    > div {
+      height: 100%;
+    }
+    .cald-block {
+      height: 130rpx;
+      width: 130rpx;
+      display: inline-block;
+      background: url('https://7863-xctest1-62e0ce-1257796632.tcb.qcloud.la/icon/rilibg.png') no-repeat;
+      background-size:100% auto;
+
+    }
+    .search-block {
+      height: 56rpx;
+      width: 130rpx;
+      display: inline-block;
+      
+      .i-search2 {
+        width: 30rpx;
+        height: 30rpx;
+        background-repeat: no-repeat;
+        background-size: auto 100%;
+      }
+    }
+
+    .banner-wrap {
+      height: 100%;
+      width: 560rpx;
+
+      image {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+
+  .hyperchannel-wrap {
+    height: 200rpx;
+    border-bottom: 1px solid #ddd;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .hyperchannel-item {
+      width: 120rpx;
+      height: 120rpx;
+      border: 1rpx solid $--color-border-gray;
+    }
+  }
+
+  .modules-wrap {
+    > div {
+      margin-top: 30rpx;
+    }
+    .index-module {
+      margin-top: 30rpx;
+    }
+
+    .im-reading {
+      .swiper-img {
+        width: 600rpx;
+        height: 360rpx;
+      }
+    }
+  }
 }
 
 </style>
